5135822c19ab54b623c2b79e785c35ad1e01020e
josemarmendes <josemarmendes17@gmail.com>
2017-09-15 13:57:17 -0300

    guardando a foto no caminho para poder salvar no banco

--- app/src/main/java/br/edu/ifma/crea_ma/FormularioInfracoesActivity.java
+++ app/src/main/java/br/edu/ifma/crea_ma/FormularioInfracoesActivity.java
@@ -1,6 +1,10 @@
 package br.edu.ifma.crea_ma;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,14 +14,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 import br.edu.ifma.crea_ma.dao.InfracaoDAO;
 import br.edu.ifma.crea_ma.modelo.Infracao;
 
 public class FormularioInfracoesActivity extends AppCompatActivity {
 
+    public static final int CODIGO_CAMERA = 567;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,28 @@ public class FormularioInfracoesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
               Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, "foto.png");
-                startActivity(intentCamera);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() +".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
+
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == CODIGO_CAMERA) {
+                ImageView foto = (ImageView) findViewById(R.id.formulario_foto_infracao);
+                Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+                Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+                foto.setImageBitmap(bitmapReduzido);
+                foto.setScaleType(ImageView.ScaleType.FIT_XY);
+            }
+        }
     }
 
     @Override
